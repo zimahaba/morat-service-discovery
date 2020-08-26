669630916ea320eeedf6313d3319ea06a16f683b
zimahaba <pmaquitorres@gmail.com>
2020-08-25 21:53:29 -0300

    first commit

---
+++ src/test/java/edu/zimahaba/morat/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.zimahaba.morat.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
